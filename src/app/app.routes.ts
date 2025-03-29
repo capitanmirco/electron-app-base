7cb962b063a9eae34161809135c1674f57f57384
mircomennonna <capitanmirco@gmail.com>
2025-03-29 17:08:33 +0100

    add routing and styles for dashboard, transactions, and reports; include Angular Material theme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {DashboardComponent} from './features/dashboard/dashboard/dashboard.component';
+import {TransactionsComponent} from './features/transactions/transactions/transactions.component';
+import {ReportsComponent} from './features/reports/reports/reports.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'transactions', component: TransactionsComponent},
+  {path: 'reports', component: ReportsComponent}
+];
